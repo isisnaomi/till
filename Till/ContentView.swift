6976b5fdad3e5c2fd22f07b40fe78e3bff73d4e0
Isis Naomi <isis@yellowme.mx>
2019-10-16 18:34:03 -0500

    Initial commit

---
+++ Till/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Till
+//
+//  Created by Isis Ramirez on 10/16/19.
+//  Copyright © 2019 Isis Ramirez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
